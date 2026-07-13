04f7fa5301a125baaa56b56db494281dd585ccf3
Pollyana Manuela Silva Rocha <pollyana.rocha@goop.com.br>
2026-07-13 09:52:51 -0300

    Separando kuber

--- UsersAPI/obj/Debug/net8.0/UsersAPI.AssemblyInfo.cs
+++ UsersAPI/obj/Debug/net8.0/UsersAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UsersAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe848105975bbffd1b13e3b3e64f529d43b8cd17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1aa7be55608f2a914799f80f4095ee93962afa1")]
 [assembly: System.Reflection.AssemblyProductAttribute("UsersAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UsersAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
