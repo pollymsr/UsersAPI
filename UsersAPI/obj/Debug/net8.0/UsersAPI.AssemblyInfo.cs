e7c543899019d54bf8344e38dbe478d8d9eb98e5
Pollyana Manuela Silva Rocha <pollyana.rocha@goop.com.br>
2026-08-15 17:17:06 -0300

    chore: update auto-generated build files

--- UsersAPI/obj/Debug/net8.0/UsersAPI.AssemblyInfo.cs
+++ UsersAPI/obj/Debug/net8.0/UsersAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UsersAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1aa7be55608f2a914799f80f4095ee93962afa1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04f7fa5301a125baaa56b56db494281dd585ccf3")]
 [assembly: System.Reflection.AssemblyProductAttribute("UsersAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UsersAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
